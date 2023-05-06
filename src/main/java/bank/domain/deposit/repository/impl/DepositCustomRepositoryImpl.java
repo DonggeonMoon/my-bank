90d083ae97a030aaa8d6141fc5f6389a312e51a8
Donggeon Moon <nullsafer@naver.com>
2023-05-07 01:47:34 +0900

    fix: jpa 추가 후 오류들 해결 중

--- src/main/java/bank/domain/deposit/repository/impl/DepositCustomRepositoryImpl.java
+++ src/main/java/bank/domain/deposit/repository/impl/DepositCustomRepositoryImpl.java
@@ -1,8 +1,8 @@
 package bank.domain.deposit.repository.impl;
 
 import bank.domain.deposit.dto.DepositDto;
-import bank.domain.deposit.dto.QDeposit;
 import bank.domain.deposit.model.Deposit;
+import bank.domain.deposit.model.QDeposit;
 import bank.domain.deposit.repository.DepositCustomRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static bank.domain.deposit.dto.QDeposit.deposit;
+import static bank.domain.deposit.model.QDeposit.deposit;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -37,7 +38,7 @@ public class DepositCustomRepositoryImpl implements DepositCustomRepository {
     }
 
     public void delete(DepositDto depositDto) {
-        jpaQueryFactory.delete(QDeposit.deposit)
+        jpaQueryFactory.delete(deposit)
                 .where(deposit.id.eq(depositDto.getId())
                         .and(deposit.amount.eq(depositDto.getAmount()))
                         .and(deposit.tenure.eq(depositDto.getTenure()))
