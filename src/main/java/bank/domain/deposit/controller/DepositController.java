50c90ea078e3acf1fbb962fe7b83ab4a8b0fed0f
DonggeonMoon <nullsafer@naver.com>
2023-05-07 16:02:13 +0900

    fix: 설정 오류 해결

--- src/main/java/bank/domain/deposit/controller/DepositController.java
+++ src/main/java/bank/domain/deposit/controller/DepositController.java
@@ -3,12 +3,18 @@ package bank.domain.deposit.controller;
 import bank.domain.deposit.dto.DepositDto;
 import bank.domain.deposit.service.DepositService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class DepositController {
     private final DepositService depositService;
+
+    @GetMapping("/test")
+    public String test() {
+        return "teest";
+    }
 
     public boolean submit() {
         return depositService.save(DepositDto.builder().build());
--- src/main/java/bank/domain/deposit/service/DepositService.java
+++ src/main/java/bank/domain/deposit/service/DepositService.java
@@ -1,11 +1,13 @@
 package bank.domain.deposit.service;
 
 import bank.domain.deposit.dto.DepositDto;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Service
 public class DepositService {
     public boolean save(DepositDto depositDto) {
         return true;
