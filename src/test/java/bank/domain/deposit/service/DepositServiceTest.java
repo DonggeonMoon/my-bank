c3dd3ed3bf8559a9253b199bdfdb712f6124d971
Donggeon Moon <nullsafer@naver.com>
2023-05-14 16:02:57 +0900

    :wrench: feat: 테스트 환경 설정

---
+++ src/test/java/bank/domain/deposit/service/DepositServiceTest.java
@@ -0,0 +1,30 @@
+package bank.domain.deposit.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {
+        "file:src/main/webapp/WEB-INF/spring/dispatcher-servlet.xml",
+        "file:src/main/webapp/WEB-INF/spring/root-context.xml"
+})
+@WebAppConfiguration
+class DepositServiceTest {
+
+    @Configuration
+    static class TestConfig {
+    }
+
+    @Autowired
+    DepositService depositService;
+
+    @Test
+    void test() {
+        depositService.test();
+        }
+}
